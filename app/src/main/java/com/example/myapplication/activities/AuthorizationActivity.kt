edb2635d9b1ab7f0ef9fc438f6dc382b61d5264e
Dmytro Kutko <dmytrokutko@gmail.com>
2019-02-19 10:06:41 +0200

    create login fragment view

--- app/src/main/java/com/example/myapplication/activities/AuthorizationActivity.kt
+++ app/src/main/java/com/example/myapplication/activities/AuthorizationActivity.kt
@@ -3,11 +3,14 @@ package com.example.myapplication.activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.R
+import com.example.myapplication.activities.fragments.LoginFragment
 
 class AuthorizationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authorization)
+
+        supportFragmentManager.beginTransaction().replace(R.id.container, LoginFragment()).commit()
     }
 }
---
+++ app/src/main/java/com/example/myapplication/activities/fragments/LoginFragment.kt
@@ -0,0 +1,34 @@
+package com.example.myapplication.activities.fragments
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.myapplication.R
+import kotlinx.android.synthetic.main.fragment_login.*
+
+class LoginFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        initListener()
+
+    }
+
+    private fun initListener() {
+        tvRegistrationFromLogin.setOnClickListener {
+            fragmentManager!!.beginTransaction().replace(R.id.container, RegistrationFragment()).commit()
+        }
+    }
+}
