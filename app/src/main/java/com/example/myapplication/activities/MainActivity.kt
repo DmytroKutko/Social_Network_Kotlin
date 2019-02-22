96dd03167677b156cf9a0b7b8eef7bd7d3f53f59
Dmytro Kutko <dmytrokutko@gmail.com>
2019-02-22 07:43:36 +0200

    create menu fragments

--- app/src/main/java/com/example/myapplication/activities/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/activities/MainActivity.kt
@@ -6,9 +6,13 @@ import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
 import android.view.MenuItem
 import com.example.myapplication.R
+import com.example.myapplication.activities.fragments.main.SettingsFragment
+import com.example.myapplication.activities.fragments.main.GalleryFragment
+import com.example.myapplication.activities.fragments.main.LatestMessagesFragment
+import com.example.myapplication.activities.fragments.main.PeoplesFragment
+import com.example.myapplication.activities.fragments.main.UserProfileFragment
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
@@ -38,41 +42,43 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
-
-    //Don't need options menu
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        menuInflater.inflate(R.menu.main, menu)
-//        return true
-//    }
-
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        when (item.itemId) {
-//            R.id.action_settings -> return true
-//            else -> return super.onOptionsItemSelected(item)
-//        }
-//    }
-
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
             R.id.nav_user -> {
-                // Handle the camera action
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.main_container, UserProfileFragment())
+                    .addToBackStack(null)
+                    .commit()
             }
             R.id.nav_gallery -> {
-
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.main_container, GalleryFragment())
+                    .addToBackStack(null)
+                    .commit()
             }
             R.id.nav_message -> {
-
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.main_container, LatestMessagesFragment())
+                    .addToBackStack(null)
+                    .commit()
             }
-            R.id.nav_friends -> {
-
+            R.id.nav_peoples -> {
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.main_container, PeoplesFragment())
+                    .addToBackStack(null)
+                    .commit()
             }
             R.id.nav_settings -> {
-
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.main_container, SettingsFragment())
+                    .addToBackStack(null)
+                    .commit()
             }
             R.id.nav_logout -> {
                 FirebaseAuth.getInstance().signOut()
